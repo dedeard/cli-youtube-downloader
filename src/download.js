38743adfa87a9cd9776b2a07d3348705c58e1592
dede ariansya <dedeariansya1@gmail.com>
2023-02-20 22:16:29 +0800

    🚅 perfs: update 5 files

--- src/download.js
+++ src/download.js
@@ -1,47 +1,47 @@
-const os = require("os");
-const ffmpeg = require("fluent-ffmpeg");
-const ytdl = require("ytdl-core");
-const sanitize = require("sanitize-filename");
-const progress = require("progress-stream");
-const cliProgress = require("cli-progress");
-const colors = require("ansi-colors");
+const ffmpeg = require('fluent-ffmpeg');
+const ytdl = require('ytdl-core');
+const sanitize = require('sanitize-filename');
+const progress = require('progress-stream');
+const cliProgress = require('cli-progress');
+const colors = require('ansi-colors');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg');
 
-class YoutubeMp3Downloader {
-  constructor(options) {
-    this.youtubeVideoQuality = options && options.youtubeVideoQuality ? options.youtubeVideoQuality : "highestaudio";
-    this.outputPath = options && options.outputPath ? options.outputPath : os.homedir();
-    this.outputOptions = options && options.outputOptions ? options.outputOptions : [];
+// Set the path for the ffmpeg library
+ffmpeg.setFfmpegPath(ffmpegPath.path);
 
-    if (options && options.ffmpegPath) {
-      ffmpeg.setFfmpegPath(options.ffmpegPath);
-    }
-  }
-
-  download(videoUrl) {
-    let self = this;
-
-    return new Promise(async (resolve, reject) => {
-      let info;
-
-      const bar = new cliProgress.SingleBar({
-        format: "{message} || " + colors.blue("{bar} {percentage}%") + " || {title} ",
-        hideCursor: true,
+/**
+ * function download(videoUrl)
+ * Downloads the video at the given URL using ytdl, as well as retrieving additional information like title and artist. Uses progress to display a progress bar while downloading.
+ *
+ * @param {string} videoUrl - The URL link of the video that is being downloaded
+ * @returns {Promise} - Returns the promise after the download is completed
+ */
+function download(videoUrl, outputPath) {
+  let self = this;
+  return new Promise(async (resolve, reject) => {
+    // Initializing progress bar
+    let info;
+    const bar = new cliProgress.SingleBar({
+      format:
+        '{message} || ' + colors.blue('{bar} {percentage}%') + ' || {title} ',
+      hideCursor: true,
+    });
+    bar.start(100, 0, { title: videoUrl });
+    try {
+      // Retrieving video info
+      bar.update(0, { message: colors.blue('Getting video informations') });
+      info = await ytdl.getInfo(videoUrl, {
+        quality: 'highestaudio',
       });
-      bar.start(100, 0, { title: videoUrl });
 
-      try {
-        bar.update(0, { message: colors.blue("Get video informations") });
-        info = await ytdl.getInfo(videoUrl, { quality: this.youtubeVideoQuality });
-      } catch (err) {
-        return reject(err);
-      }
+      // Retrieving title and artist based on the video's title
       const videoTitle = sanitize(info.videoDetails.title);
-      let artist = "Unknown";
-      let title = "Unknown";
-      const thumbnail = info.videoDetails.thumbnails ? info.videoDetails.thumbnails[0].url : info.videoDetails.thumbnail || null;
 
-      if (videoTitle.indexOf("-") > -1) {
-        let temp = videoTitle.split("-");
+      let artist;
+      let title;
+
+      if (videoTitle.indexOf('-') > -1) {
+        let temp = videoTitle.split('-');
         if (temp.length >= 2) {
           artist = temp[0].trim();
           title = temp[1].trim();
@@ -51,62 +51,68 @@ class YoutubeMp3Downloader {
       }
       bar.update(0, { title: videoTitle });
 
-      const fileName = (videoTitle || info.videoId) + ".mp3";
+      // Creating file name for the video
+      const fileName = (videoTitle || info.videoId) + '.mp3';
 
-      // Stream setup
-      const streamOptions = {
-        quality: self.youtubeVideoQuality,
-        requestOptions: self.requestOptions,
-      };
+      // Stream
+      const stream = ytdl.downloadFromInfo(info, {
+        quality: 'highestaudio',
+      });
 
-      const stream = ytdl.downloadFromInfo(info, streamOptions);
-
-      stream.on("error", function (err) {
+      // Error handling
+      stream.on('error', function (err) {
         return reject(err);
       });
 
-      stream.on("response", function (httpResponse) {
+      // Start of response
+      stream.on('response', function (httpResponse) {
         // Setup of progress module
         const str = progress({
-          length: parseInt(httpResponse.headers["content-length"]),
+          length: parseInt(httpResponse.headers['content-length']),
           time: self.progressTimeout,
         });
 
-        // Add progress event listener
-        bar.update(0, { message: colors.blue("Downloading") });
-        str.on("progress", function (progress) {
+        // Adding progress event listener
+        bar.update(0, { message: colors.blue('Downloading') });
+        str.on('progress', function (progress) {
           bar.update(progress.percentage);
         });
 
-        let outputOptions = ["-id3v2_version", "4", "-metadata", "title=" + title, "-metadata", "artist=" + artist];
-        if (self.outputOptions) {
-          outputOptions = outputOptions.concat(self.outputOptions);
-        }
+        // This code adds the command line options for an audio file
+        const outputOptions = ['-id3v2_version', '4'];
 
-        const audioBitrate = info.formats.find((format) => !!format.audioBitrate).audioBitrate;
+        // Add 'title' to outputOptions if present
+        if (title) outputOptions.concat(['-metadata', 'title=' + title]);
 
-        // Start encoding
-        new ffmpeg({
-          source: stream.pipe(str),
-        })
+        // Add 'artist' to outputOptions if present
+        if (artist) outputOptions.concat(['-metadata', 'artist=' + artist]);
+
+        const audioBitrate = info.formats.find(
+          (format) => !!format.audioBitrate
+        ).audioBitrate;
+
+        // Starting encoding
+        new ffmpeg({ source: stream.pipe(str) })
+          .withAudioCodec('libmp3lame')
           .audioBitrate(audioBitrate || 192)
-          .withAudioCodec("libmp3lame")
-          .toFormat("mp3")
+          .toFormat('mp3')
           .outputOptions(...outputOptions)
-          .on("error", (err) => {
-            bar.update(0, { message: colors.red("Failed") });
+          .on('error', (err) => {
+            bar.update(0, { message: colors.red('Failed') });
             bar.stop();
             return reject(err);
           })
-          .on("end", () => {
-            bar.update(100, { message: colors.green("Success") });
+          .on('end', () => {
+            bar.update(100, { message: colors.green('Success') });
             bar.stop();
             return resolve();
           })
-          .saveToFile(self.outputPath + fileName);
+          .saveToFile(outputPath + fileName);
       });
-    });
-  }
+    } catch (err) {
+      return reject(err);
+    }
+  });
 }
 
-module.exports = YoutubeMp3Downloader;
+module.exports = download;
--- src/index.js
+++ src/index.js
@@ -3,56 +3,62 @@ const inquirer = require('inquirer');
 const ytUrl = require('youtube-url');
 const ytfps = require('ytfps');
 const c = require('ansi-colors');
-const Ytmp3 = require('./download');
-const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+const download = require('./download');
 
 // Download a song from youtube
 const DownloadAFromYoutube = async (v = false) => {
+  // Check if the URL is invalid, if so print an error message
   if (v) {
     console.log(c.red('URL is invalid.') + '\n');
   }
+
+  // Create an inquirer dialogue asking for a URL
   const answer = await inquirer.prompt({
     name: 'url',
     type: 'input',
     message: 'Paste the URL:',
   });
 
+  // Check if the URL is valid and replace music.youtube.com with youtube.com
   const url = answer.url.replace('music.youtube.com', 'youtube.com');
-
   if (!ytUrl.valid(url)) {
     return DownloadAFromYoutube(true);
   }
 
+  //Create directory if it doesn't exist
   const outputPath = './';
   if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath, { recursive: true });
   }
-  const YD = new Ytmp3({
-    ffmpegPath: ffmpeg.path, // FFmpeg binary location
-    outputPath,
-  });
 
+  // Try downloading the file, catch any errors
   try {
-    await YD.download(url);
+    await download(url, outputPath);
   } catch (e) {
     console.error(e);
     process.exit(0);
   }
+
+  // Call main
   console.log('\n');
   main();
 };
 
 // Download playlist from youtube
 const DownloadPlaylistFromYoutube = async (v = false) => {
+  // Check validity of URL and alert to error if URL is invalid
   if (v) {
     console.log(c.red('Error : ' + v) + '\n');
   }
+
+  // Create an inquirer dialogue asking for a URL
   const answer = await inquirer.prompt({
     name: 'url',
     type: 'input',
     message: 'Paste the URL:',
   });
 
+  // Check if the URL is valid and replace music.youtube.com with youtube.com
   const url = answer.url.replace('music.youtube.com', 'youtube.com');
   let pl;
   try {
@@ -61,29 +67,29 @@ const DownloadPlaylistFromYoutube = async (v = false) => {
     return DownloadPlaylistFromYoutube(e.message);
   }
 
+  // Create a directory with the playlist title, if it doesn't exist
   const outputPath = './' + pl.title + '/';
   if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath, { recursive: true });
   }
-  const YD = new Ytmp3({
-    ffmpegPath: ffmpeg.path, // FFmpeg binary location
-    outputPath,
-  });
 
+  // Iterate over each video in the playlist, trying to download them
   for (let i in pl.videos) {
     try {
-      await YD.download(pl.videos[i].url);
+      await download(pl.videos[i].url, outputPath);
     } catch (e) {
       console.log(e.message);
     }
   }
 
+  // Call main
   console.log('\n');
   main();
 };
 
-// Main
+// Main function handles user input
 async function main() {
+  // Ask user which action to perform with list input
   const answer = await inquirer.prompt({
     name: 'type',
     type: 'list',
@@ -95,6 +101,7 @@ async function main() {
     ],
   });
 
+  // Switch based on user input to call respective function
   switch (answer.type) {
     case 'Download a song from youtube':
       DownloadAFromYoutube();
@@ -108,4 +115,5 @@ async function main() {
   }
 }
 
+// Initialize main Function
 main();
